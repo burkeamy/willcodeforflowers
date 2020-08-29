ddc3964eac4fa910bc505a8eb1c52ed04502a2bc
Amy Burke <burkeamy30@gmail.com>
2020-08-29 16:37:59 -0400

    added a responsive menu. Still a work in progress.

---
+++ main.js
@@ -0,0 +1,9 @@
+function mobileNavMenu () {
+    let mobileNavDisplay = document.getElementById('mobileNavDisplay');
+     if(mobileNavDisplay.style.display === 'block') {
+        mobileNavDisplay.style.display = 'none'
+    } else {
+        mobileNavDisplay.style.display = 'block';
+    }
+}
+
